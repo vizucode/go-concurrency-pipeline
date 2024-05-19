c35d935df5e505259ac6d7eb3c0d3dbc70612477
vizucode <havisikkubaru@gmail.com>
2024-05-19 15:51:14 +0700

    feat: concurrent seeder

---
+++ concurrent-pattern-archive/fileSeeder/file_seeder.go
@@ -0,0 +1,59 @@
+package fileseeder
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+type File struct {
+	*os.File
+}
+
+func createFile(nFile int, targetFolder string) <-chan File {
+	chanFileOut := make(chan File)
+
+	go func(out chan File) {
+		defer close(chanFileOut)
+		for i := 0; i < nFile; i++ {
+			file, _ := os.Create(fmt.Sprintf("%s/text-%d.text", targetFolder, i))
+			out <- File{File: file}
+		}
+	}(chanFileOut)
+
+	return chanFileOut
+}
+
+func writeFile(chanFileIn <-chan File) {
+	var words = []string{
+		"lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipiscing", "elit",
+		"sed", "do", "eiusmod", "tempor", "incididunt", "ut", "labore", "et", "dolore",
+		"magna", "aliqua", "ut", "enim", "ad", "minim", "veniam", "quis", "nostrud",
+		"exercitation", "ullamco", "laboris", "nisi", "ut", "aliquip", "ex", "ea",
+		"commodo", "consequat", "duis", "aute", "irure", "dolor", "in", "reprehenderit",
+		"in", "voluptate", "velit", "esse", "cillum", "dolore", "eu", "fugiat", "nulla",
+		"pariatur", "excepteur", "sint", "occaecat", "cupidatat", "non", "proident",
+		"sunt", "in", "culpa", "qui", "officia", "deserunt", "mollit", "anim", "id",
+		"est", "laborum",
+	}
+
+	for file := range chanFileIn {
+		go func(file File) {
+			var sb strings.Builder
+			for j := 0; j < 5000; j++ {
+				word := words[int(time.Now().Unix())%len(words)]
+				sb.WriteString(word)
+			}
+			file.Write([]byte(sb.String()))
+			defer file.Close()
+		}(file)
+	}
+}
+
+func ExecSeeder(nFile int, targetFolder string) {
+	chanFile := createFile(nFile, targetFolder)
+	writeFile(chanFile)
+	writeFile(chanFile)
+	writeFile(chanFile)
+}
--- concurrent-pattern-archive/main.go
+++ concurrent-pattern-archive/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	seeder "concurrentarchive/fileSeeder"
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
 
+func main() {
+	err := os.MkdirAll("files", 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	now := time.Now()
+
+	seeder.ExecSeeder(3000, "files")
+
+	since := time.Since(now)
+	fmt.Printf("Executed in %f second", since.Seconds())
 }
